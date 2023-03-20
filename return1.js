ed2416e5696f40e675d737d8fd88bd99571add53
dear <dear@fresnoU.org>
2023-03-20 09:53:16 -0700

    Chapter3notes

---
+++ return1.js
@@ -0,0 +1,37 @@
+// const readLine = require(`readline-sync`);
+// //1
+// function nameSwapper (){
+// let first = readLine.question("first name");
+//  let last = readLine.question("last name");
+//  return (`${last}, ${first}`);
+//   }
+//
+//  //2
+//  function generateNum(){
+//   let num1 = readLine.question("Enter a digit: ");
+//   let num2 = readLine.question("Enter another digit: ");
+//   return num1+num2;
+//  }
+// console.log(generateNum());
+// console.log(generateNum());
+//
+// //3
+//  function avgTemp(){
+//    let temp1 = readLine.question("Day 1: ");
+//    let temp2 = readLine.question("Day 2: ");
+//     let temp3 = readLine.question("Day 3: ");
+//     let temp4 = readLine.question("Day 4: ");
+//      let temp5 = readLine.question("Day 5: ");
+//     let temp6 = readLine.question("Day 6: ");
+//      let temp7 = readLine.question("Day 7: ");
+//  }
+//   let total = (temp1+temp2+temp3+temp4+temp5+temp6+temp7)/7
+//     return( total.toFixed(2));
+//
+//  }
+//  console.log(averagetemperature())
+//
+//
+// Charity Collection Revisited
+//  function sum(num1, num2, num3, num4){
+//      console.log(`${num1} + ${num2} + ${num3} = ${num4}`);
